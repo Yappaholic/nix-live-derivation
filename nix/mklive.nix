3384f1fa8973a1f1aaeadfe8b32499bb6ad6675d
Yappaholic <sav.boyar@gmail.com>
2026-02-17 03:00:27 +0300

    six test bruh

--- nix/mklive.nix
+++ nix/mklive.nix
@@ -7,6 +7,7 @@
   nixLiveDerivation,
 }: let
   newSrcText = pkgs.runCommand "get-new-source" {} ''
+    mkdir -p $out
     ${nixLiveDerivation}/bin/nix-live-derivation ${gitString} > $out/live-derivation-info.json
   '';
   newSrc = builtins.fromJSON (builtins.readFile "${newSrcText}/live-derivation-info.json");
