9560f35af0ebb3668ef95415c1deab19d7ffb033
Yappaholic <sav.boyar@gmail.com>
2026-02-17 03:06:08 +0300

    seVENN test bruh

--- nix/mklive.nix
+++ nix/mklive.nix
@@ -11,12 +11,22 @@
     ${nixLiveDerivation}/bin/nix-live-derivation ${gitString} > $out/live-derivation-info.json
   '';
   newSrc = builtins.fromJSON (builtins.readFile "${newSrcText}/live-derivation-info.json");
-  fetcher = import ./get-fetcher.nix {
-    inherit pkgs;
-    fetcherString = newSrc.fetcher;
-  };
+  fetcherString = newSrc.fetcher;
+  fetcher =
+    if fetcherString == "fetchFromGitHub"
+    then pkgs.fetchFromGitHub
+    else if fetcherString == "fetchFromSourcehut"
+    then pkgs.fetchFromSourcehut
+    else if fetcherString == "fetchFromBitBucket"
+    then pkgs.fetchFromBitBucket
+    else if fetcherString == "fetchFromGitLab"
+    then pkgs.fetchFromGitLab
+    else if fetcherString == "fetchGit"
+    then pkgs.fetchgit
+    else throw "Unexpected fetcher type";
 in
   package.overrideAttrs (_: _: {
+    version = "git";
     src = fetcher {
       owner = newSrc.owner;
       repo = newSrc.repo;
